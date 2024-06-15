30118a91be022118f15e5feebb0ed1016e9f4df4
FrandyAquino <frandyjavieraquino13@gmail.com>
2024-06-15 14:28:33 -0400

    Creando el primer commit (Subiendo todos los archivos).

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import '@/styles/MainDesign/App.css'
+import Navbar from './components/MainLayout/Navbar'
+import Sidebar from './components/MainLayout/Sidebar'
+import Home from './components/Home/Home'
+import Settings from './components/Settings/Settings'
+import Reports from './components/Reports/Reports'
+import Products from './components/Products/Products'
+import Invoice from './components/Invoice/Invoice'
+import Clients from './components/Clients/Clients'
+
+
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Sidebar />
+          <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/clients" element={<Clients />} />
+          <Route path="/invoice" element={<Invoice />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+          </div>
+        </div>
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MainLayout/Sidebar.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaHome, FaBox, FaUsers, FaFileInvoice, FaChartBar, FaCog } from 'react-icons/fa';
+import styles from '@/styles/MainLayout/Sidebar.module.css';
+
+function Sidebar() {
+    const [activeLink, setActiveLink] = useState("");
+
+    return (
+        <div className={styles.sidebar}>
+            <div className={styles.sidebarHeader}>
+                Sidebar
+            </div>
+            <ul className={styles.sidebarMenu}>
+                <li className={activeLink === "/" ? "active pressed" : ""}>
+                    <Link to="/" className={styles.sidebarLink} onClick={() => setActiveLink("/")}>
+                        <FaHome className={styles.sidebarIcon} /> Home
+                    </Link>
+                </li>
+                <li className={activeLink === "/products" ? "active pressed" : ""}>
+                    <Link to="/products" className={styles.sidebarLink} onClick={() => setActiveLink("/products")}>
+                        <FaBox className={styles.sidebarIcon} /> Products
+                    </Link>
+                </li>
+                <li className={activeLink === "/clients" ? "active pressed" : ""}>
+                    <Link to="/clients" className={styles.sidebarLink} onClick={() => setActiveLink("/clients")}>
+                        <FaUsers className={styles.sidebarIcon} /> Clients
+                    </Link>
+                </li>
+                <li className={activeLink === "/invoice" ? "active pressed" : ""}>
+                    <Link to="/invoice" className={styles.sidebarLink} onClick={() => setActiveLink("/invoice")}>
+                        <FaFileInvoice className={styles.sidebarIcon} /> Invoice
+                    </Link>
+                </li>
+                <li className={activeLink === "/reports" ? "active pressed" : ""}>
+                    <Link to="/reports" className={styles.sidebarLink} onClick={() => setActiveLink("/reports")}>
+                        <FaChartBar className={styles.sidebarIcon} /> Reports
+                    </Link>
+                </li>
+                <li className={activeLink === "/settings" ? "active pressed" : ""}>
+                    <Link to="/settings" className={styles.sidebarLink} onClick={() => setActiveLink("/settings")}>
+                        <FaCog className={styles.sidebarIcon} /> Settings
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Sidebar;
