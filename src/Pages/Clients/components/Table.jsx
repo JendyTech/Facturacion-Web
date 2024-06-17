7d796c59ce7bc51126893f9a97d28780507f820a
FrandyAquino <frandyjavieraquino13@gmail.com>
2024-06-17 00:06:16 -0400

    Se agrego el mainlayout

---
+++ src/Pages/Clients/components/Table.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Table() {
+    return (
+        <div>
+            <h1 style={{color: "red"}}>HOLA MUNDO</h1>
+        </div>
+    )
+}
+
+export default Table
--- src/Pages/Settings/Settings.jsx
+++ src/Pages/Settings/Settings.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import MainLayout from '@/Pages/MainLayout/MainLayout'
 
 function Settings() {
   return (
-    <div>Settings</div>
+    <div>
+      <MainLayout>
+        <h2>Settings</h2>
+      </MainLayout>
+    </div>
   )
 }
 
