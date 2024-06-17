3891d2778944af7142b58879bdb8303c0d94c526
FrandyAquino <frandyjavieraquino13@gmail.com>
2024-06-17 16:49:32 -0400

    Arreglando la tabla de producto y Cliente

--- src/Pages/Clients/components/Table.jsx
+++ src/Pages/Clients/components/Table.jsx
@@ -1,58 +1,72 @@
 import { useState, useEffect } from 'react';
-import DataTable from "react-data-table-component";
-import { getAllClients } from '@/database/GET/getAllClients'
-
+import DataTable from 'react-data-table-component';
+import { getAllClients } from '@/database/GET/getAllClients';
 
 function Table() {
-
-    const ExpandedComponent = ({ data }) => <pre>{JSON.stringify(data, null, 2)}</pre>;
-
-    const [clients, setClients] = useState([])
+    const [clients, setClients] = useState([]);
 
     useEffect(() => {
-      const getData = async () => {
-        const data = await getAllClients();
-        setClients(data)
-      }
-  
-      getData()
-    }, [])
+        const getData = async () => {
+            const data = await getAllClients();
+            setClients(data);
+        };
+
+        getData();
+    }, []);
 
     const columns = [
         {
             name: 'ID',
-            selector: row => row.id,
+            selector: (row) => row.id,
+            width: '50px', 
         },
         {
             name: 'Nombre',
-            selector: row => row.name_client,
+            selector: (row) => row.name_client,
+            width: '175px',
         },
         {
             name: 'Apellido',
-            selector: row => row.lastname_client,
+            selector: (row) => row.lastname_client,
+            width: '175px',
         },
         {
             name: 'Identificacion',
-            selector: row => row.identification_client,
+            selector: (row) => row.identification_client,
+            width: '120px',
         },
         {
             name: 'Telefono',
-            selector: row => row.phonenumber_client,
+            selector: (row) => row.phonenumber_client,
+            width: '120px',
         },
         {
             name: 'Correo',
-            selector: row => row.email_client,
+            selector: (row) => row.email_client,
+            width: '200px',
+            cell: (row) => (
+                <div style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                    {row.email_client}
+                </div>
+            ),
         },
         {
             name: 'Direccion',
-            selector: row => row.address_client,
+            selector: (row) => row.address_client,
+            width: '175px',
+            cell: (row) => (
+                <div style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                    {row.address_client}
+                </div>
+            ),
         },
         {
             name: 'Fecha de Creacion',
-            selector: row => row.created_date_client,
+            selector: (row) => row.created_date_client,
+            width: '140px',
+
         },
     ];
-
 
     return (
         <div>
@@ -61,14 +75,13 @@ function Table() {
                 data={clients}
                 pagination
                 paginationPerPage={7}
-                paginationRowsPerPageOptions= {[3, 4, 5, 6, 7]}
+                paginationRowsPerPageOptions={[3, 4, 5, 6, 7]}
                 highlightOnHover
-                selectableRows  
-                expandableRows
-			    expandableRowsComponent={ExpandedComponent}
+                selectableRows
+                style={{ flex: 1 }} // Enable flex for proportional distribution
             />
         </div>
-    )
+    );
 }
 
-export default Table
+export default Table;
--- src/Pages/Products/components/Table.jsx
+++ src/Pages/Products/components/Table.jsx
@@ -1,49 +1,67 @@
 import { useState } from 'react';
 import { useEffect } from 'react';
 import DataTable from 'react-data-table-component';
-import { getAllProducts } from '@/database/GET/getAllProducts'
+import { getAllProducts } from '@/database/GET/getAllProducts';
 
 export default function Table() {
-
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
       const data = await getAllProducts();
-      setProducts(data)
-    }
+      setProducts(data);
+    };
 
-    getData()
-  }, [])
+    getData();
+  }, []);
 
   const columns = [
     {
       name: 'Id',
-      selector: row => row.id,
+      selector: (row) => row.id,
+      width: '50px',
     },
     {
       name: '',
-      selector: row => row.image_product,
+      selector: (row) => row.image_product,
+      width: '100px',
+      cell: (row) => (
+        <img
+          src={row.image_product}
+          alt="Product Image"
+          style={{ width: '100%' }}
+        />
+      ),
     },
     {
-      name: 'Nombre',
-      selector: row => row.name_product,
+      name: 'Nombre del producto',
+      selector: (row) => row.name_product,
+      width: '225px',
     },
     {
       name: 'Descripcion',
-      selector: row => row.description_product,
+      selector: (row) => row.description_product,
+      width: '375px',
+      cell: (row) => (
+        <div style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>
+          {row.description_product}
+        </div>
+      ),
     },
     {
       name: 'Precio',
-      selector: row => row.price_product,
+      selector: (row) => row.price_product,
+      width: '120px', 
     },
     {
       name: 'Marca',
-      selector: row => row.brand_product,
+      selector: (row) => row.brand_product,
+      width: '150px',
     },
     {
       name: 'Cantidad',
-      selector: row => row.stock_product,
+      selector: (row) => row.stock_product,
+      width: '100px',
     },
   ];
 
@@ -54,10 +72,13 @@ export default function Table() {
         data={products}
         pagination
         paginationPerPage={7}
-        paginationRowsPerPageOptions= {[3, 4, 5, 6, 7]}
+        paginationRowsPerPageOptions={[3, 4, 5, 6, 7]}
         highlightOnHover
         selectableRows
+        style={{ flex: 1 }}
       />
     </div>
-  )
+  );
 }
+
+
