c8f0a7481dc2aa5611e5ac1823496a89c9bd0d7e
Jeriel <jgomezinf19@gmail.com>
2024-06-17 17:39:46 -0400

    varios

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,11 +1,13 @@
 import MainLayout from "../MainLayout/MainLayout"
 import Table from "./components/Table"
+import TableHeader from "./components/TableHeader"
 
 function Products() {
   return (
     <div>
       <MainLayout>
         <h2>Products</h2>
+        <TableHeader />
         <Table />
       </MainLayout>
     
---
+++ src/Pages/Products/components/TableHeader.jsx
@@ -0,0 +1,8 @@
+
+export default function TableHeader() {
+    return (
+        <div className="d-flex flex-row bd-highlight justify-content-around align-items-center">
+            
+        </div>
+    )
+}
